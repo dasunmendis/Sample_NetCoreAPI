2d32c988168235072543538898be9520f412490b
dasunmendis <dasunmendis@gmail.com>
2021-07-12 12:13:39 +0530

    initial commit

---
+++ RestaurantAPI/RestaurantAPI/Models/OrderMaster.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestaurantAPI.Models
+{
+    public class OrderMaster
+    {
+        [Key]
+        public long OrderMasterId { get; set; }
+        [Column(TypeName = "nvarchar(75)")]
+        public string OrderNumber { get; set; }
+        public int CustomerId { get; set; }
+        [Column(TypeName = "nvarchar(10)")]
+        public string PaymentMethod { get; set; }
+        public decimal GrandTotal { get; set; }
+
+        //Navigation Properties
+        public Customer Customer { get; set; }
+        public List<OrderDetail> OrderDetails { get; set; }
+    }
+}
